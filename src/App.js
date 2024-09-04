4d7ac6d53410f6b890b1cbc8621451f6da7c4546
rajshakya123 <rajendrasingh4719@gmail.com>
2024-09-04 09:20:13 +0530

    add download excel feature
    ref#main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import { Row, Col, Select, Button } from 'antd';
+import { Row, Col, Select, Button, Carousel } from 'antd';
 import { translateText } from './store/translationSlice';
 import { languages } from './constant/languages';
 import { speechToVoice } from './store/speechToVoiceSlice';
-const stringSimilarity = require('string-similarity');
+import * as XLSX from 'xlsx';
+// const stringSimilarity = require('string-similarity');
 const { Option } = Select;
 
 const App = () => {
@@ -14,114 +15,93 @@ const App = () => {
   const { audioUrl } = useSelector((state) => state.speechToVoice);
   const [allTranscript, setAllTranscript] = useState([]);
   const [translatedSentences, setTranslatedSentences] = useState([]);
+  const [displayedSentences, setDisplayedSentences] = useState([]);
   const [translatedLanguage, setTranslatedLanguage] = useState('en');
   const [recognezedLanguage, setRecognezedLanguage] = useState('hi');
   const [isRecording, setIsRecording] = useState(false);
   const recognitionRef = useRef(null);
   const isSpeakingRef = useRef(false);
   const queueRef = useRef([]);
-  const transcriptsEndRef = useRef(null);
-  const translatedEndRef = useRef(null);
+  // const transcriptsEndRef = useRef(null);
+  // const translatedEndRef = useRef(null);
   const processedSentences = useRef(new Set());
   const accumulatedTextRef = useRef('');
   const isRecordingRef = useRef(isRecording);
   const interimTranscriptRef = useRef('');
-
+  console.log(allTranscript, 'allTranscript')
   useEffect(() => {
     isRecordingRef.current = isRecording;
   }, [isRecording]);
 
   useEffect(() => {
-    if (translatedText?.translated_text) {
+    if (translatedText?.translated_text && displayedSentences !== undefined && translatedSentences !== null) {
       setTranslatedSentences((prevSentences) => [
         ...prevSentences,
-        translatedText.translated_text,
+        translatedText.translated_text
       ]);
+      setDisplayedSentences((prevSentences) => [
+        ...prevSentences,
+        translatedText.translated_text
+      ])
       queueRef.current.push(translatedText.translated_text);
     }
   }, [translatedText]);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (false) {
-        speakNextSentence();
-      }
-    }, 1000);
-    return () => clearInterval(intervalId);
-  }, []);
-
-  useEffect(() => {
-    if (allTranscript.length > 0) {
-      transcriptsEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    if (displayedSentences?.length > 5) {
+      const sentences = displayedSentences.slice(1)
+      setDisplayedSentences(sentences);
     }
-  }, [allTranscript]);
+  }, [displayedSentences]);
 
-  useEffect(() => {
-    if (translatedSentences.length > 0) {
-      translatedEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [translatedSentences]);
+  console.log(displayedSentences, 'displayedSentences', translatedSentences, 'translatedSentences')
+  // useEffect(() => {
+  //   if (allTranscript.length > 0) {
+  //     transcriptsEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  //   }
+  // }, [allTranscript]);
 
-  const extractSentences = (text, onSentenceMatch, processedSentences, isFinal = false) => {
-    const sentenceEndings = [
-      'है', 'हूँ', 'थे', 'हों', 'होगा', 'होगी', 'था', 'थी', 'रहा है', 'रही है',
-      // add more sentence endings as needed
-    ];
+  // useEffect(() => {
+  //   if (translatedSentences.length > 0) {
+  //     translatedEndRef?.current?.scrollLeft = translatedEndRef?.current?.scrollWidth;
+  //   }
+  // }, [translatedSentences]);
 
-    const regex = new RegExp(`(${sentenceEndings.join('|')})(?=[\\s\\n]|$)`, 'g');
+  // const extractSentences = (text, onSentenceMatch, processedSentences, isFinal = false) => {
+  //   const sentenceEndings = [
+  //     'है', 'हूँ', 'थे', 'हों', 'होगा', 'होगी', 'था', 'थी', 'रहा है', 'रही है',
+  //     // add more sentence endings as needed
+  //   ];
 
-    let match;
-    let lastIndex = 0;
-    let pendingText = accumulatedTextRef.current; // Text that didn’t form a complete sentence last time
-    while ((match = regex.exec(text)) !== null) {
-      const endIndex = regex.lastIndex;
-      const sentence = text.slice(lastIndex, endIndex).trim();
-      lastIndex = endIndex;
-      if (sentence && !processedSentences.has(sentence)) {
-        const fullSentence = pendingText + ' ' + sentence; // Combine with pending text
-        pendingText = ''; // Clear pending text after use
-        onSentenceMatch(fullSentence.trim()); // Pass the combined sentence
-        processedSentences.add(fullSentence);
-      }
-    }
+  //   const regex = new RegExp(`(${sentenceEndings.join('|')})(?=[\\s\\n]|$)`, 'g');
 
-    const remainingText = text.slice(lastIndex).trim();
+  //   let match;
+  //   let lastIndex = 0;
+  //   let pendingText = accumulatedTextRef.current; // Text that didn’t form a complete sentence last time
+  //   while ((match = regex.exec(text)) !== null) {
+  //     const endIndex = regex.lastIndex;
+  //     const sentence = text.slice(lastIndex, endIndex).trim();
+  //     lastIndex = endIndex;
+  //     if (sentence && !processedSentences.has(sentence)) {
+  //       const fullSentence = pendingText + ' ' + sentence; // Combine with pending text
+  //       pendingText = ''; // Clear pending text after use
+  //       onSentenceMatch(fullSentence.trim()); // Pass the combined sentence
+  //       processedSentences.add(fullSentence);
+  //     }
+  //   }
 
-    if (isFinal && remainingText && !processedSentences.has(remainingText)) {
-      const fullSentence = pendingText + ' ' + remainingText; // Append remaining to pending
-      onSentenceMatch(fullSentence.trim());
-      processedSentences.add(fullSentence);
-      pendingText = ''; // Clear pending text after use
-    }
-  };
+  //   const remainingText = text.slice(lastIndex).trim();
 
-  const processTranscript = (transcript, isFinal = false) => {
-    const handleNewSentences = (sentence) => {
-      const lastElement = [...processedSentences.current];
-      if (sentence?.length > 10) {
-        if (lastElement?.length > 0) {
-          const similarity = stringSimilarity?.compareTwoStrings(lastElement.pop(), sentence);
-          if (sentence && !(similarity >= 8) && !processedSentences.current.has(sentence)) {
-            accumulatedTextRef.current = '';
-            setAllTranscript((prev) => [...prev, sentence]);
-            processedSentences.current.add(sentence);
-            dispatch(translateText({ text: sentence, lang: translatedLanguage }));
-          }
-        }
-      }
-      else {
-        accumulatedTextRef.current = sentence;
-      }
-
-    };
-
-    extractSentences(transcript, handleNewSentences, processedSentences.current, isFinal);
-  };
+  //   if (isFinal && remainingText && !processedSentences.has(remainingText)) {
+  //     const fullSentence = pendingText + ' ' + remainingText; // Append remaining to pending
+  //     onSentenceMatch(fullSentence.trim());
+  //     processedSentences.add(fullSentence);
+  //     pendingText = ''; // Clear pending text after use
+  //   }
+  // };
 
   const initializeRecognition = async () => {
     try {
-      await navigator.mediaDevices.getUserMedia({ audio: true });
-
       const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
       if (!Recognition) {
         console.error('SpeechRecognition API is not supported in this browser.');
@@ -148,9 +128,8 @@ const App = () => {
         }
 
         if (finalTranscript) {
-          processTranscript(finalTranscript, true);
-        } else if (interimTranscriptRef.current) {
-          processTranscript(interimTranscriptRef.current);
+          setAllTranscript((prev) => [...prev, finalTranscript]);
+          dispatch(translateText({ text: finalTranscript, lang: translatedLanguage }));
         }
       };
 
@@ -206,6 +185,26 @@ const App = () => {
     }
   };
 
+  const downloadSpeechAsExcel = () => {
+    // Prepare the data for the Excel sheet
+  const wsData = translatedSentences.map((sentence) => [sentence]);
+
+  // Create a new worksheet with the data
+  const ws = XLSX.utils.aoa_to_sheet(wsData);
+
+  // Set the width of the first column (A)
+  const wscols = [{ wch: 50 }]; // 'wch' is the width of the column in characters
+  ws['!cols'] = wscols;
+
+  // Create a new workbook and append the worksheet
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, 'Translated Sentences');
+
+  // Trigger the download
+  XLSX.writeFile(wb, 'translatedSentences.xlsx');
+};
+
+
   const toggleRecording = () => {
     if (isRecording) {
       if (recognitionRef.current) {
@@ -218,6 +217,7 @@ const App = () => {
       initializeRecognition(translatedLanguage);
       setAllTranscript([]);
       setTranslatedSentences([]);
+      setDisplayedSentences([]);
       queueRef.current = [];
     }
     setIsRecording(!isRecording);
@@ -233,55 +233,78 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Speech to Speech Translation</h1>
       <Row gutter={16}>
-        <Col xs={24} sm={12} md={8} lg={8} xl={8}>
-          <h2>Recognized Text:</h2>
-          <div className="transcripts">
-            {allTranscript.length > 0 && allTranscript.map((item, index) => <p key={index}>{item}</p>)}
-            <div ref={transcriptsEndRef} />
+        <Col xs={24} sm={24} md={12} lg={12} xl={12} >
+          <div>
+            <img src='./Capture.PNG'></img>
           </div>
         </Col>
-        <Col xs={24} sm={12} md={8} lg={8} xl={8}>
+        <Col xs={24} sm={24} md={12} lg={12} xl={12} className="languageFields">
+          <div className="select-container">
+            {/* <h2>Recognized Language</h2> */}
+            <Select
+              label
+              onChange={onChangeRecognizedLanguage}
+              id="recognizedlanguage"
+              placeholder="Select Target Language"
+              defaultValue="hi"
+              style={{ width: '100%' }} // Ensure it takes the full width of the container
+            >
+              {languages.map(item => (
+                <Option key={item.id} value={item.code}>
+                  {item.name}
+                </Option>
+              ))}
+            </Select>
+          </div>
+          <div className="select-container">
+            {/* <h2>Translated Language</h2> */}
+            <Select
+              onChange={onChangeTranslatedLanguage}
+              id="translatedlanguage"
+              placeholder="Select Target Language"
+              defaultValue="en"
+              style={{ width: '100%' }} // Ensure it takes the full width of the container
+            >
+              {languages.map(item => (
+                <Option key={item.id} value={item.code}>
+                  {item.name}
+                </Option>
+              ))}
+            </Select>
+          </div>
           <div className="button-container">
             <Button onClick={toggleRecording} type="primary">
               {isRecording ? 'Stop Recording' : 'Start Recording'}
             </Button>
           </div>
-          <div className="select-container">
-          <h2>Recognized Language:</h2>
-            <Select style={{ width: 128 }} onChange={onChangeRecognizedLanguage} id="Recognizedlanguage" placeholder="Select Target Language" defaultValue="hi">
-              {languages.map(item => <Option key={item.id} value={item.code}>{item.name}</Option>)}
-            </Select>
-          </div>
-          <div className="select-container">
-          <h2>Translated Language:</h2>
-            <Select style={{ width: 128 }} onChange={onChangeTranslatedLanguage} id="Translatedlanguage" placeholder="Select Target Language" defaultValue="en">
-              {languages.map(item => <Option key={item.id} value={item.code}>{item.name}</Option>)}
-            </Select>
-          </div>
-          <div className="audio-container">
-            <h2>Audio Output:</h2>
-            {audioUrl ? (
-              <audio
-                src={`http://localhost:5000/audio/${audioUrl.split('/').pop()}`}
-                controls
-                autoPlay
-                onEnded={() => isSpeakingRef.current = false} // Reset speaking flag when audio ends
-                onError={(e) => console.error('Audio playback error:', e)}
-              />
-            ) : (
-              'Audio is not coming?'
-            )}
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={24} xl={24} style={{ position: 'relative' }}>
+          <button onClick={downloadSpeechAsExcel} className="copy-button">Copy</button>
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={24} xl={24} style={{ position: 'relative' }}>
+          <div className="scrollable-text-container">
+            <div className="scrollable-text">
+              <div className="scroll-content">
+                {displayedSentences.map((sentence, index) => (
+                  <span key={index} className="carousel-item">
+                    {sentence}
+                  </span>
+                ))}
+              </div>
+            </div>
           </div>
         </Col>
-        <Col xs={24} sm={12} md={8} lg={8} xl={8}>
-          <h2>Translated Text:</h2>
-          <div className="scrollable-text">
-            {translatedSentences.map((sentence, index) => (
-              <p key={index}>{sentence}</p>
-            ))}
-            <div ref={translatedEndRef} />
+        <Col xs={24} sm={24} md={24} lg={24} xl={24} className='container-audio'>
+          <div className="audio">
+            {/* <h2>Audio Output:</h2> */}
+            <audio
+              src={`http://localhost:5000/audio/${audioUrl?.split('/').pop()}`}
+              controls
+              autoPlay
+              onEnded={() => isSpeakingRef.current = false} // Reset speaking flag when audio ends
+              onError={(e) => console.error('Audio playback error:', e)}
+            />
           </div>
         </Col>
       </Row>
